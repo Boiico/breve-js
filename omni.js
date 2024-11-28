02b9a39343df5ea73df525a6241c98df71b234f2
Boiico <160245478+Boiico@users.noreply.github.com>
2024-11-28 12:26:07 +0330

    Create omni.js

---
+++ omni.js
@@ -0,0 +1,19 @@
+// Omni.js - A lightweight JavaScript library by Paul Bakhtiari
+
+// Shortcut for document.getElementById
+const di = (id) => document.getElementById(id);
+
+// Shortcut for document.write
+const dw = (content) => document.write(content);
+
+// Shortcut for console.log
+const log = (message) => console.log(message);
+
+// Shortcut for alert
+const al = (message) => alert(message);
+
+// Shortcut for prompt
+const prm = (message) => prompt(message);
+
+// Shortcut for numeric prompt
+const prmNum = (message) => +prompt(message);
